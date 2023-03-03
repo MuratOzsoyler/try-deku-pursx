0dda6af814498214a48b3408f04f8db7dac22f07
murat <murat.ozsoyler@gmail.com>
2023-03-03 14:31:47 +0300

    Non trivial pursx use

--- src/Main.purs
+++ src/Main.purs
@@ -3,23 +3,35 @@ module Main where
 import Prelude
 
 import Debug (spy)
-import Deku.Control (blank, text_)
+import Deku.Control (blank, text_, (<#~>))
 import Deku.Core (fixed)
 import Deku.Pursx ((~~))
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
+import Effect.Aff (Milliseconds(..), delay, launchAff_)
+import Effect.Class (liftEffect)
+import FRP.Event (burning, create)
 import Type.Proxy (Proxy(..))
 
-tableData ∷ Array { code ∷ Int, explanation ∷ String }
-tableData = [ { code: 1, explanation: "Expl 1" }, { code: 23, explanation: "Expl 23" } ]
+-- tableData ∷ Array { code ∷ Int, explanation ∷ String }
+-- tableData = [ { code: 1, explanation: "Expl 1" }, { code: 23, explanation: "Expl 23" } ]
 
 main :: Effect Unit
-main = runInBody
-  ( table_ ~~
-      { rows: fixed $ tableData <#> \{ code, explanation } ->
-          row_ ~~ { code: fixed [ text_ $ spy "code" $ show code ], explanation: fixed [ text_ explanation ] }
-      }
-  )
+main = do
+  { push: setTableData, event: tableData' } <- create
+  { event: tableData } <- burning { code: 1, explanation: "Expl 1" } tableData'
+  launchAff_ do
+    delay $ Milliseconds 100.0
+    liftEffect $ setTableData { code: 23, explanation: "Expl 23" }
+
+  runInBody
+    ( table_ ~~
+        { rows: fixed
+            [ tableData <#~> \{ code, explanation } ->
+                row_ ~~ { code: fixed [ text_ $ spy "code" $ show code ], explanation: fixed [ text_ explanation ] }
+            ]
+        }
+    )
 
 row_ =
   Proxy
